d8fc6097380500fff4b77248f3abd6fa618c8cd7
sunxuexiang <616338444@qq.com>
2022-11-27 14:57:12 +0800

    修改配置

---
+++ bill-common/src/test/java/com/cloudfly/start/bill/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudfly.start.bill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ bill-gather/src/main/java/com/cloudfly/start/bill/GatherApplication.java
@@ -0,0 +1,13 @@
+package com.cloudfly.start.bill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatherApplication.class, args);
+	}
+
+}
