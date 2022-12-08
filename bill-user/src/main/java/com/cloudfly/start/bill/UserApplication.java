f043f33f185ad67b96a764b4f24786332cd7ac24
git <postmaster@yxttxs.com>
2022-12-08 16:50:20 +0800

    update name

--- bill-user/src/main/java/com/cloudfly/start/bill/UserApplication.java
+++ bill-user/src/main/java/com/cloudfly/start/bill/UserApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SelfApplication {
+public class UserApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SelfApplication.class, args);
+		SpringApplication.run(UserApplication.class, args);
 	}
 
 }
